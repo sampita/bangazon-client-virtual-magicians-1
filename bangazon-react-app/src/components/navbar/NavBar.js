d246726f4859f7e7c5f0981111a2142d790c2dc9
Sam Pita <samanthakpita@gmail.com>
2020-02-24 15:23:07 -0600

    removed bullet points from nav

--- bangazon-react-app/src/components/navbar/NavBar.js
+++ bangazon-react-app/src/components/navbar/NavBar.js
@@ -18,16 +18,16 @@ class NavBar extends Component {
                     <ul className="navbar flex">
                         {/* { */}
                         {/* (this.props.user) ? */}
-                            <li><Link className="nav-link" to="/sell">Sell</Link></li>
+                            <li className="nav"><Link className="nav-link" to="/sell">Sell</Link></li>
                             {/* : null} */}
-                        <li><Link className="nav-link" to="/buy">Buy</Link></li>
+                        <li className="nav"><Link className="nav-link" to="/buy">Buy</Link></li>
                         <input type="text" placeholder="Search product keywords..."></input>
                         {/* Ternary statement */}
                         {/* { */}
                         {/* (this.props.user) ? */}
-                            <li><Link className="nav-link" to="/mycart">Shopping Cart</Link></li>
+                            <li className="nav"><Link className="nav-link" to="/mycart">Shopping Cart</Link></li>
                             {/* : null} */}
-                        <li><Link className="nav-link" to="/myprofile">My Profile</Link></li>
+                        <li className="nav"><Link className="nav-link" to="/myprofile">My Profile</Link></li>
                     </ul>
                 </nav>
             </header>

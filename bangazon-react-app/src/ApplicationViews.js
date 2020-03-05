cf13cbeadf1a8d69ff7e99c28e529b82ab65e24a
Julian Garcia <garcia.j.julian@gmail.com>
2020-03-05 14:23:25 -0600

    added the details view for the products

--- bangazon-react-app/src/ApplicationViews.js
+++ bangazon-react-app/src/ApplicationViews.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {Route, Redirect} from "react-router-dom";
 import Home from './components/home/Home.js'
 import SellForm from './components/home/ProductForm'
+import ProductDetail from './components/home/ProductDetails'
 import Location from './components/home/Location'
 import Register from './components/auth/register.js';
 import UserProfile from './components/home/UserProfile.js';
@@ -45,11 +46,18 @@ class ApplicationViews extends Component {
                 <Route exact path="/sell" render={(props) => {
                     if (this.props.isAuthenticated()) {
                         return <SellForm 
-                            {...props}/>
+                                {...props}/>
                         } else {
                             return <Redirect to="/login"/>
                         }
                 }} />
+
+                <Route exact path="/product/:productId(\d+)" render={props => {
+                    return <ProductDetail 
+                                {...props} 
+                                // addToOrder= {this.props.addToOrder}
+                                />
+                }}/>
 
                 <Route exact path="/location" render={(props) => {
                     return <Location />
---
+++ bangazon-react-app/src/components/home/ProductDetails.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import APIManager from "../../modules/APIManager"
+
+class ProductDetail extends Component {
+    // also this contains the form to add an itinerary item
+    state = {
+        product: []
+    }
+
+    componentDidMount() {
+        this.getProduct()
+    }
+
+    getProduct = () => {
+        APIManager.get("products", this.props.match.params.productId)
+            .then((product) => {
+                this.setState({ product: product })
+            })
+    }
+
+    // handleCartAdd = () => {
+    //     this.props.addToOrder(this.state.product.id)
+
+    // }
+
+    render() {
+        const { name, id, price, description } = this.state.product;
+
+        return (
+            <>
+                <article>
+                    <div>
+                        <div>
+                            <div>
+                                <a href={`/product/${id}`}>
+                                    {name}
+                                </a>
+                            </div>
+                            <div>
+                                <h2>${price}</h2>
+                            </div>
+                        </div>
+                        <p>{description}</p>
+                        {/* <button onClick={this.handleCartAdd}>
+                            Add to Cart
+                        </button> */}
+                    </div>
+                </article>
+            </>
+        )
+    }
+}
+
+export default ProductDetail
